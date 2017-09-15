59c312c6d45b4033931c723e8b81a1657ed5ede6
Michael Levy <mike.levy.work@gmail.com>
2017-09-15 13:47:05 -0600

    MEX driver constructs column for init_marbl
    The MEX code builds and runs again, but the next step will be to update the mex
    driver so that matlab can provide arrays for delta_z, zw, and zt instead of
    relying on the hard-coded column.

--- mex_marbl_driver.F90
+++ mex_marbl_driver.F90
@@ -32,6 +32,11 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
   character(len=640) message
   integer status, init_result, nt, str_cnt
 
+  ! Allocatable array for storing MARBL domain information
+  ! (layer thickness, interface depth, and layer center depth)
+  integer :: nlev
+  real*8, allocatable, dimension(:) :: delta_z, zw, zt
+
   ! Allocatable array for storing MARBL log
   character(len=512), allocatable :: str_array(:)
   integer :: str_ind
@@ -57,18 +62,28 @@ subroutine mexFunction(nlhs, plhs, nrhs, prhs)
       if (status .ne. 0) &
         call mexPrintf('Error calling put_setting()')
     case ('init')
+      ! pack marbl domain information into arrays
+      nlev = 5
+      allocate(delta_z(nlev), zt(nlev), zw(nlev))
+      delta_z = 1.
+      zw = (/1., 2., 3., 4., 5./)
+      zt = (/0.5, 1.5, 2.5, 3.5, 4.5/)
+
+      ! call init_marbl()
       call mexPrintf('MEX-file note: calling init\n')
-      ! Call the computational subroutine.
-      init_result = init_marbl(nt)
-      if (init_result .ne. 0) then
-        call mexPrintf('MEX-file note: initialization failed!\n')
-      else
+      init_result = init_marbl(delta_z, zw, zt, nlev, nt)
+      deallocate(delta_z, zw, zt)
+
+      ! Return tracer count (unless init_marbl returned an error)
+      if (init_result .eq. 0) then
         ! Return tracer count
         plhs(1) = mxCreateDoubleMatrix(1,1,0)
         y_ptr = mxGetPr(plhs(1))
         size = 1
         nt_r8 = nt
         call mxCopyReal8ToPtr(nt_r8,y_ptr,size)
+      else
+        call mexPrintf('MEX-file note: initialization failed!\n')
       end if
 #if 0
     case ('surface')
